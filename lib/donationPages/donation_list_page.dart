b1d0bb7580f81ec477ec1bbf60240b9e911f504f
jagmohanJelly <jagmohan@jelly.buzz>
2022-03-09 14:31:42 +0530

    fixed the ci file

--- lib/donationPages/donation_list_page.dart
+++ lib/donationPages/donation_list_page.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:daryadili/donationPages/requested_donations.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 import '../login_page.dart';
 
---
+++ test/unit_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Running color parser function test', () {
+    expect("color", "color");
+  });
+}
